51117a4d6e86ecbadafe24489bba18d38a2f35a6
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2025-03-21 20:39:48 +0000

    denoised frame

--- tp_deteccion/frame.py
+++ tp_deteccion/frame.py
@@ -28,6 +28,27 @@ def apply_color_convertion(frame, color):
     return cv2.cvtColor(frame, color)
 
 
-def threshold_frame(frame, slider_max, binary, trackbar_value):
+def get_threshold_frame(frame, slider_max, binary, trackbar_value):
     _, th = cv2.threshold(frame, trackbar_value, slider_max, binary)
     return th
+
+
+"""
+    Applies morphological operations to denoise (remove noise) an image by removing small noise and filling small holes.
+
+    @param frame: The input frame (grayscale or binary).
+    @param kernel_size: The size of the structuring element (square kernel).
+    
+    @return: A denoised image where small noise is removed and small holes are filled.
+
+"""
+
+
+def get_denoised_frame(frame, method, kernel_size):
+    kernel = cv2.getStructuringElement(
+        method, (kernel_size, kernel_size))
+    # remove small noise with erosion and restore main structure with dilation
+    opening = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
+    # Fill small holes with dilation and remove noise with erosion
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    return closing
--- tp_deteccion/shapes_detection.py
+++ tp_deteccion/shapes_detection.py
@@ -1,6 +1,6 @@
 import cv2
 import config
-from frame import handle_video_capture, video_capture_read, check_frame_exit, apply_color_convertion, threshold_frame
+from frame import handle_video_capture, video_capture_read, check_frame_exit, apply_color_convertion, get_threshold_frame, get_denoised_frame
 from trackbar import create_trackbar, get_trackbar_pos
 
 
@@ -10,26 +10,41 @@ def main():
 
     capture = handle_video_capture(main_window_name, config.VIDEO_PATH)
 
+    # Threshold trackbar
     trackbar_thresh_name = 'Threshold'
     thresh_slider_max = 255
     create_trackbar(trackbar_thresh_name, main_window_name,
                     slider_default=60, slider_max=thresh_slider_max)
 
+    # Threshold trackbar
+    trackbar_kernel_name = 'Kernel denoise'
+    contour_kernel_max = 10
+    create_trackbar(trackbar_kernel_name, main_window_name,
+                    slider_default=1, slider_max=contour_kernel_max)
+
     while True:
         main_frame = video_capture_read(capture)
 
-        monochromatic_frame = apply_color_convertion(
+        # Apply monocromatic
+        binary_frame = apply_color_convertion(
             frame=main_frame, color=cv2.COLOR_RGB2GRAY)
 
+        # Apply threshold
         trackbar_thresh_value = get_trackbar_pos(
             trackbar_thresh_name, main_window_name)
 
-        binary_frame = threshold_frame(frame=monochromatic_frame, slider_max=thresh_slider_max,
-                                       binary=cv2.THRESH_BINARY,
-                                       trackbar_value=trackbar_thresh_value)
+        threshold_frame = get_threshold_frame(frame=binary_frame, slider_max=thresh_slider_max,
+                                              binary=cv2.THRESH_BINARY,
+                                              trackbar_value=trackbar_thresh_value)
 
+        # Apply noise reduction
+        trackbar_denoise_value = get_trackbar_pos(
+            trackbar_kernel_name, main_window_name)
+        
+        denoised_frame = get_denoised_frame(frame=threshold_frame, method=cv2.MORPH_ELLIPSE, kernel_size=trackbar_denoise_value)
+        
         cv2.imshow(main_window_name, main_frame)
-        cv2.imshow(binary_window_name, binary_frame)
+        cv2.imshow(binary_window_name, denoised_frame)
         if check_frame_exit():
             break
 
