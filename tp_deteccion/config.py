7756ab8c62ebd75dc3d5c63e4ef8df9096dd50e4
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2025-03-22 21:54:58 +0000

    dynamic camera by OS

--- tp_deteccion/config.py
+++ tp_deteccion/config.py
@@ -1,4 +1,24 @@
+import platform
 # config.py - Configuration file for capture path
 
 # RTMP stream URL
-VIDEO_PATH = "/dev/video0"
+WSL_CAMERA = "/dev/video0"
+WINDOWS_CAMERA = 0
+
+def detect_os():
+    system = platform.system()
+    if system == "Windows":
+        return "windows"
+    elif system == "Darwin":
+        return "mac"
+    elif "microsoft" in platform.uname().release.lower():  # Detect WSL
+        return "wsl"
+    else:
+        return "linux"
+    
+def choose_camera_by_OS():
+    os = detect_os()
+    if os == "windows":
+        return WINDOWS_CAMERA
+    else:
+        return WSL_CAMERA
--- tp_deteccion/shapes_detection.py
+++ tp_deteccion/shapes_detection.py
@@ -1,25 +1,26 @@
 import cv2
 import config
+from config import choose_camera_by_OS
 from frame import handle_video_capture, video_capture_read, check_frame_exit, apply_color_convertion, get_threshold_frame, get_denoised_frame
 from trackbar import create_trackbar, get_trackbar_pos
 
 
 def main():
-    main_window_name = 'Form Detection'
-    binary_window_name = 'Binary'
+    main_frame_name = 'Form Detection'
+    processed_frame_name = 'Processed Detection'
 
-    capture = handle_video_capture(main_window_name, config.VIDEO_PATH)
+    capture = handle_video_capture(main_frame_name, choose_camera_by_OS())
 
     # Threshold trackbar
     trackbar_thresh_name = 'Threshold'
     thresh_slider_max = 255
-    create_trackbar(trackbar_thresh_name, main_window_name,
+    create_trackbar(trackbar_thresh_name, main_frame_name,
                     slider_default=60, slider_max=thresh_slider_max)
 
     # Threshold trackbar
     trackbar_kernel_name = 'Kernel denoise'
     contour_kernel_max = 10
-    create_trackbar(trackbar_kernel_name, main_window_name,
+    create_trackbar(trackbar_kernel_name, main_frame_name,
                     slider_default=1, slider_max=contour_kernel_max)
 
     while True:
@@ -31,7 +32,7 @@ def main():
 
         # Apply threshold
         trackbar_thresh_value = get_trackbar_pos(
-            trackbar_thresh_name, main_window_name)
+            trackbar_thresh_name, main_frame_name)
 
         threshold_frame = get_threshold_frame(frame=binary_frame, slider_max=thresh_slider_max,
                                               binary=cv2.THRESH_BINARY,
@@ -39,12 +40,12 @@ def main():
 
         # Apply noise reduction
         trackbar_denoise_value = get_trackbar_pos(
-            trackbar_kernel_name, main_window_name)
+            trackbar_kernel_name, main_frame_name)
         
         denoised_frame = get_denoised_frame(frame=threshold_frame, method=cv2.MORPH_ELLIPSE, kernel_size=trackbar_denoise_value)
-        
-        cv2.imshow(main_window_name, main_frame)
-        cv2.imshow(binary_window_name, denoised_frame)
+
+        cv2.imshow(main_frame_name, main_frame)
+        cv2.imshow(processed_frame_name, denoised_frame)
         if check_frame_exit():
             break
 
